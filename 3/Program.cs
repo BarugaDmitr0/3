1c2f7026692b8c9b660509f4220d19eee8f88064
BarugaDmitr0 <BarugaDmitr0@DESKTOP-B33V7U7>
2023-05-09 01:08:41 +0300

    Добавьте файлы проекта.

---
+++ 3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PR5
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string text = "Текст, який потрібно перевірити";
+            int brackets = 0;
+
+            foreach (char c in text)
+            {
+                if (c == '(')
+                {
+                    brackets++;
+                }
+
+                else if (c == ')')
+                {
+                    brackets--;
+                }
+                
+                if (brackets < 0)
+                {
+                    Console.WriteLine("Дужки розставленi неправильно!");
+                  
+                    return;
+                }
+
+            }
+
+            if (brackets == 0)
+            {
+                Console.WriteLine("Дужки розставленi правильно!");
+            }
+
+            else
+            {
+                Console.WriteLine("Дужки розставленi неправильно!");
+            }
+        }
+    }
+}
